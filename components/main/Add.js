0f486c6c9dda93e6cb3179d24a6c9ebafd36bb82
hoang-cloud <hoangdl201096@gmail.com>
2021-01-28 21:17:39 +0700

    second commit

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,15 +1,92 @@
-import React, { Component } from 'react'
-import {View,Text} from 'react-native'
-export class Add extends Component {
-    render() {
-        return (
-            <View>
-                <Text>
-                    Add Component
-                </Text>
-            </View>
-        )
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker'
+export default function Add() {
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+      const galleryStatus = await ImagePicker.requestCameraPermissionsAsync()
+      setHasGalleryPermission(galleryStatus.status === 'granted');
+    })();
+  }, []);
+
+    const takePicture = async () => {
+        if(camera){
+          const data = await camera.takePictureAsync(null)
+          console.log(data.uri)
+          setImage(data.uri)
+        }
     }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+    })
+    console.log(result)
+    if(!result.cancelled){
+      setImage(result.uri)
+    }
+  }
+
+  if (hasCameraPermission === null || hasGalleryPermission === false) {
+    return <View />;
+  }
+  if (hasCameraPermission === false || hasGalleryPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={{flex:1}}>
+      <View style={styles.cameraContainer}>
+      <Camera
+      ref={ref => setCamera(ref)}  
+      style={styles.fixRatio} 
+      type={type} 
+      ratio={'4:3'} />
+      </View>
+      <Button 
+            style={{
+              flex: 0.2,
+              alignSelf: 'flex-end',
+              alignItems:'center'
+            }}
+            title="Flip Image"
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+              )
+            }}
+          />
+      <Button
+        title="Take a picture"
+        onPress={() => takePicture()}
+      />
+      <Button
+        title="Pick image from gallery"
+        onPress={() => pickImage()}
+      />
+      <Image source={{uri: image}} style={{flex:1}}/>
+    </View>
+  );
 }
 
-export default Add
+const styles = StyleSheet.create({
+    cameraContainer: {
+      flex:1
+    },
+    fixRatio: {
+      flex:1
+    }
+})
